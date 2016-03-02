8a12292ccb19a3095796612a0bc8a8d263999de7
Dustin <dustinsinkey1@gmail.com>
2016-03-01 19:23:42 -0800

    Added LoDash and JQuery

--- test/hello_spec.js
+++ test/hello_spec.js
@@ -3,6 +3,6 @@ var sayHello = require('../src/hello');
 describe("Hello", function(){
 
   it("says Hello", function(){
-    expect(sayHello()).toBe("Hello, World!");
+    expect(sayHello('Jane')).toBe("Hello, Jane!");
   });
 });
