db863dbbb34162777d421804fff27db9ae3eb3fa
Dustin <dustinsinkey1@gmail.com>
2016-03-01 19:48:20 -0800

    Added test for scope

---
+++ test/scope_spec.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var Scope = require('../src/scope');
+
+describe("Scope", function(){
+
+  it("can be construncted and used as and object", function(){
+    var scope = new Scope();
+    scope.aProperty = 1;
+
+    expect(scope.aProperty).toBe(1);
+  });
+  
+});
