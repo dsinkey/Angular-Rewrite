d03ac7bf03afdd78ffb7eb8cf6fed270cc665b9c
Dustin <dustinsinkey1@gmail.com>
2016-03-02 21:45:08 -0800

    Deleted test

--- src/scope.js
+++ src/scope.js
@@ -1,7 +1,15 @@
 'use strict';
 
 function Scope() {
+  this.$$watcher = [];
+}
 
+Scope.prototype.$watch = function(watchFn, listenerFn){
+  var watcher = {
+    watchFn: watchFn,
+    listenerFn: listenerFn
+  };
+  this.$$watchers.push(watcher);
 };
 
 module.exports = Scope;
